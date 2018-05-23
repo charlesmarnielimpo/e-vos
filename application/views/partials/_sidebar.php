a8b05da1d00445fba400386397d601ffc82b14e9
Charles Marnie Limpo <charlesmarnielimpo@gmail.com>
2018-05-23 16:02:48 +0800

    Updated File Structure

---
+++ application/views/partials/_sidebar.php
@@ -0,0 +1,51 @@
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav slimscrollsidebar">
+                <div class="sidebar-head">
+                    <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span></h3>
+                </div>
+                <div class="user-profile">
+                    <div class="dropdown user-pro-body">
+                        <div>
+                            <img src="plugins/images/users/varun.jpg" alt="user-img" class="img-circle">
+                        </div>
+                        <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Steave Gection
+                        </a>
+                    </div>
+                </div>
+                <ul class="nav" id="side-menu">
+                    <li class="active">
+                        <a href="index.html" class="waves-effect active"><i class="mdi mdi-view-dashboard fa-fw"></i> <span class="hide-menu">Dashboard</span></a>
+                    </li>
+                    <li>
+                        <a href="academicyear.html" class="waves-effect"><i class="mdi mdi-school fa-fw"></i> <span class="hide-menu">Academic Year</span></a>
+                    </li>
+                    <li>
+                        <a href="result.html" class="waves-effect"><i class="mdi mdi-chart-bar fa-fw"></i> <span class="hide-menu">Result</span></a>
+                    </li>
+                    <li>
+                        <a href="candidates.html" class="waves-effect"><i class="mdi mdi-walk fa-fw"></i> <span class="hide-menu">Candidates</span></a>
+                    </li>
+                    <li>
+                        <a href="positions.html" class="waves-effect"><i class="mdi mdi-seal fa-fw"></i> <span class="hide-menu">Positions</span></a>
+                    </li>
+                    <li>
+                        <a href="partylists.html" class="waves-effect"><i class="mdi mdi-tooltip-text fa-fw"></i> <span class="hide-menu">Partylists</span></a>
+                    </li>
+                    <li>
+                        <a href="voters.html" class="waves-effect"><i class="mdi mdi-grease-pencil fa-fw"></i> <span class="hide-menu">Voters</span></a>
+                    </li>
+                    <li>
+                        <a href="records.html" class="waves-effect"><i class="mdi mdi-record-rec fa-fw"></i> <span class="hide-menu">Records</span></a>
+                    </li>
+                    <li>
+                        <a href="reports.html" class="waves-effect"><i class="mdi mdi-reorder-horizontal fa-fw"></i> <span class="hide-menu">Reports</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar -->
+        <!-- ============================================================== -->
